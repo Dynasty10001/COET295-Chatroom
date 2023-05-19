1155e1ec15f55e2e33a63192c8c2baaa2433d9ba
Kyle Doerksen <doerksen8556@saskpolyech.ca>
2023-05-19 16:19:41 -0600

    Created the boiler plate server.js as well as installed some npm packages.

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bp = require('body-parser');
+const session = require('express-session');
+const PORT = process.env.PORT || 9000;
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+const path = require('path');
+
+app.use(express.static('public'));
+app.use(bp.urlencoded({extended: true}));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    unset: 'destroy'
+}));
+
+app.set('view engine', 'ejs');
+console.log(`http://localhost: ${PORT}`);
+
+
+
+app.get('/', async(req, res) => {
+    res.render('index.ejs', {name: 'kyle'});
+});
+
+
+server.listen(9000, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
